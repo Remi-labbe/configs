6465e072b8d6482104ff50872555e7123afa2f38
Remi-labbe <labbe.remi@free.fr>
2021-02-08 13:34:04 +0100

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
+
 set fish_greeting
 set TERM "xterm-256color"
 set EDITOR "nvim"
